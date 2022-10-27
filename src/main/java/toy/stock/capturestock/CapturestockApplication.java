a1315e2279a1f8ba10a06381025277b36977e9d2
topKim <bighit.top@gmail.com>
2022-10-27 23:28:43 +0900

    init backend

---
+++ src/main/java/toy/stock/capturestock/CapturestockApplication.java
@@ -0,0 +1,13 @@
+package toy.stock.capturestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapturestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapturestockApplication.class, args);
+	}
+
+}
